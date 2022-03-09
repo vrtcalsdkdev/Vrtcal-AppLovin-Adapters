491e39f1545cb2adbbd4db7003be685f772aff69
Scott McCoy <scotthmccoy@gmail.com>
2022-03-08 23:43:59 -0800

    feat: Added logging
    Also:
    Default width and height when unset for Vrtcal-as-primary banners
    Bumped version number

--- VRTBannerCustomEventAppLovin.m
+++ VRTBannerCustomEventAppLovin.m
@@ -12,12 +12,13 @@
 @implementation VRTBannerCustomEventAppLovin
 
 - (void) loadBannerAd {
+    VRTLogWhereAmI();
     NSString *maAdUnitId = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"adUnitId"];
     NSString *strWidth = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"width"];
     NSString *strHeight = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"height"];
     
-    CGFloat width = [strWidth respondsToSelector:@selector(floatValue)] ? [strWidth floatValue] : 0;
-    CGFloat height = [strWidth respondsToSelector:@selector(floatValue)] ? [strHeight floatValue] : 0;
+    CGFloat width = [strWidth respondsToSelector:@selector(floatValue)] ? [strWidth floatValue] : 320;
+    CGFloat height = [strWidth respondsToSelector:@selector(floatValue)] ? [strHeight floatValue] : 50;
     
     self.maAdView = [[MAAdView alloc] initWithAdUnitIdentifier:maAdUnitId];
     self.maAdView.frame = CGRectMake(0, 0, width, height);
@@ -26,6 +27,7 @@
 }
 
 - (UIView*) getView {
+    VRTLogWhereAmI();
     return self.maAdView;
 }
 
@@ -33,35 +35,43 @@
 #pragma mark - MAAdViewAdDelegate
 
 - (void)didClickAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventClicked];
 }
 
 - (void)didDisplayAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventShown];
 }
 
 - (void)didFailToDisplayAd:(nonnull MAAd *)ad withError:(nonnull MAError *)error {
     // No Analog
+    VRTLogWhereAmI();
 }
 
 - (void)didFailToLoadAdForAdUnitIdentifier:(nonnull NSString *)adUnitIdentifier withError:(nonnull MAError *)error {
+    VRTLogWhereAmI();
     VRTError *vrtError = [VRTError errorWithCode:VRTErrorCodeCustomEvent message:error.message];
     [self.customEventLoadDelegate customEventFailedToLoadWithError:vrtError];
 }
 
 - (void)didHideAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventDidDismissModal:VRTModalTypeUnknown];
 }
 
 - (void)didLoadAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventLoadDelegate customEventLoaded];
 }
 
 - (void)didCollapseAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventDidDismissModal:VRTModalTypeUnknown];
 }
 
 - (void)didExpandAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventDidPresentModal:VRTModalTypeUnknown];
 }
 
--- VRTInterstitialCustomEventAppLovin.m
+++ VRTInterstitialCustomEventAppLovin.m
@@ -10,6 +10,7 @@
 @implementation VRTInterstitialCustomEventAppLovin
 
 - (void) loadInterstitialAd {
+    VRTLogWhereAmI();
     NSString *maAdUnitId = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"adUnitId"];
     self.maIntersatitialAd = [[MAInterstitialAd alloc] initWithAdUnitIdentifier:maAdUnitId];
     self.maIntersatitialAd.delegate = self;
@@ -17,34 +18,41 @@
 }
 
 - (void) showInterstitialAd {
+    VRTLogWhereAmI();
     [self.maIntersatitialAd showAd];
 }
 
 #pragma mark - MAAdDelegate
 - (void)didLoadAd:(MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventLoadDelegate customEventLoaded];
 }
 
 
 - (void)didClickAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventClicked];
 }
 
 - (void)didDisplayAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventDidPresentModal:VRTModalTypeInterstitial];
     [self.customEventShowDelegate customEventShown];
 }
 
 - (void)didFailToDisplayAd:(nonnull MAAd *)ad withError:(nonnull MAError *)error {
     // No Analog
+    VRTLogWhereAmI();
 }
 
 - (void)didFailToLoadAdForAdUnitIdentifier:(nonnull NSString *)adUnitIdentifier withError:(nonnull MAError *)error {
+    VRTLogWhereAmI();
     VRTError *vrtError = [VRTError errorWithCode:VRTErrorCodeCustomEvent message:error.message];
     [self.customEventLoadDelegate customEventFailedToLoadWithError:vrtError];
 }
 
 - (void)didHideAd:(nonnull MAAd *)ad {
+    VRTLogWhereAmI();
     [self.customEventShowDelegate customEventDidDismissModal:VRTModalTypeUnknown];
 }
 
